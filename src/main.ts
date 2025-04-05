e5a2d669213674994bee4c76f9e98fd9d3dedcf4
zhang <zhangwh754@163.com>
2025-04-05 11:44:52 +0800

    feat: 初始化

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+   <canvas id="gameCanvas"></canvas>
+`;
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import type { UserConfig } from "vite";
+
+export default {
+  server: {
+    host: "0.0.0.0",
+    port: 16642,
+    open: true,
+  },
+} satisfies UserConfig;
